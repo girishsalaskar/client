85155758dd68903663d9fb60064e11ebf0044bf2
Girish Salaskar <girishsalaskar812@gmail.com>
2022-11-11 17:52:46 +0530

    Code for fetching get data from node server added.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ENDPOINT } from './env';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      testAPIRes : 'No message'
+    }
+  }
+
+  callAPI = () => {
+    console.log("API Called");
+    fetch( ENDPOINT + "/testAPI" )
+      .then(res => res.text())
+      .then(res => { console.log(res); this.setState({testAPIRes : res});});
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> {this.state.testAPIRes} </p>
       </div>
     );
   }
